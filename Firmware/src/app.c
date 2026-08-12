7c9cf0957d079d4dbb77610e55d9a5b0948dafc8
dawidoczek <97553303+dawidoczek@users.noreply.github.com>
2026-08-12 20:42:06 +0200

    Implement BLE disable-by-default and hourly/NFC wakeups

--- Firmware/src/app.c
+++ Firmware/src/app.c
@@ -25,6 +25,44 @@ RAM int16_t temperature;
 // Settings
 extern settings_struct settings;
 
+RAM uint32_t ble_enable_timeout = 0;
+RAM uint8_t ble_is_enabled = 1;
+
+_attribute_ram_code_ void update_ble_state(void) {
+    uint8_t should_be_enabled = 0;
+    struct date_time t = get_time();
+    
+    // 1. Time-window condition (XX:00:00 to XX:02:00)
+    if (t.tm_min == 0 && t.tm_sec < 120) {
+        should_be_enabled = 1;
+    }
+
+    // 2. NFC tap condition (NFC_IRQ goes LOW when tapped)
+    if (!gpio_read(NFC_IRQ)) {
+        ble_enable_timeout = get_unix_time() + 120; // Keep BLE on for 2 minutes after tap
+    }
+
+    if (ble_enable_timeout > 0 && get_unix_time() < ble_enable_timeout) {
+        should_be_enabled = 1;
+    }
+
+    if (ble_get_connected()) {
+        should_be_enabled = 1; // Don't drop active connections
+    }
+
+    if (should_be_enabled != ble_is_enabled) {
+        ble_is_enabled = should_be_enabled;
+        bls_ll_setAdvEnable(ble_is_enabled);
+        
+        // Blink green when turning ON
+        if (ble_is_enabled) {
+            set_led_color(2);
+            WaitMs(50);
+            set_led_color(0);
+        }
+    }
+}
+
 _attribute_ram_code_ void user_init_normal(void)
 {                            // this will get executed one time after power up
     random_generator_init(); // must
@@ -39,6 +77,9 @@ _attribute_ram_code_ void user_init_normal(void)
 
     // epd_display_tiff((uint8_t *)bart_tif, sizeof(bart_tif));
     // epd_display(3334533);
+    
+    // Give user a 2-minute window to connect after reset
+    ble_enable_timeout = get_unix_time() + 120;
 }
 
 _attribute_ram_code_ void user_init_deepRetn(void)
@@ -52,6 +93,7 @@ _attribute_ram_code_ void main_loop(void)
 {
     blt_sdk_main_loop();
     handler_time();
+    update_ble_state();
 
     // Read battery & temperature less often when nobody is connected
     uint32_t sensor_interval = ble_get_connected() ? 30 : 300;
@@ -88,6 +130,15 @@ _attribute_ram_code_ void main_loop(void)
     }
     else
     {
+        if (!ble_is_enabled) {
+            cpu_set_gpio_wakeup(NFC_IRQ, 0, 1); // wake on NFC_IRQ low
+            bls_pm_setWakeupSource(PM_WAKEUP_PAD | PM_WAKEUP_TIMER);
+            // Wake up 1 time per second to update clock
+            bls_pm_setAppWakeupLowPower(clock_time() + CLOCK_16M_SYS_TIMER_CLK_1S, 1);
+        } else {
+            bls_pm_setWakeupSource(PM_WAKEUP_PAD);
+            bls_pm_setAppWakeupLowPower(0, 0); // Disable custom timer wakeup
+        }
         blt_pm_proc();
     }
 }
